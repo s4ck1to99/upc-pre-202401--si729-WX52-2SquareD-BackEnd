c71ed39fc0a1c6ff035acafde5e58d9bf9ee0706
MiguelC <sackito992@gmail.com>
2024-05-27 17:25:17 -0500

    chore: initial commit

---
+++ src/main/java/com/mathplayopen/system/SystemMultiContextApplication.java
@@ -0,0 +1,13 @@
+package com.mathplayopen.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemMultiContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemMultiContextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mathplayopen/system/SystemMultiContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathplayopen.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemMultiContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
